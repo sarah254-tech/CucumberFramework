3605b7da1a3a5ad72207e4aa3103682b42754a23
sbushaj <69921436+sbushaj93@users.noreply.github.com>
2025-02-22 13:13:19 -0500

    Lesson05 - Part II

--- src/test/java/com/neotech/pages/PersonalDetailsPageElements.java
+++ src/test/java/com/neotech/pages/PersonalDetailsPageElements.java
@@ -1,5 +1,7 @@
 package com.neotech.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -23,7 +25,7 @@ public class PersonalDetailsPageElements {
 	@FindBy(id = "licenseNo")
 	public WebElement drivingLicense;
 	
-	@FindBy(id = "smoker")
+	@FindBy(xpath = "//label[text()='Smoker']")
 	public WebElement smokerCheckBox;
 	
 	@FindBy(id = "emp_gender")
@@ -31,6 +33,36 @@ public class PersonalDetailsPageElements {
 	
 	@FindBy(xpath = "//div[@id='emp_gender_inputfileddiv']/div/input")
 	public WebElement genderInput; //used to click on the gender dropdown
+	
+	@FindBy(xpath = "//div[@id='emp_gender_inputfileddiv']//li")
+	public List<WebElement> genderOptions;
+	
+	@FindBy(xpath = "//div[@id='nation_code_inputfileddiv']//input")
+	public WebElement nationalityInput; //used to click on the nationality dropdown
+	
+	@FindBy(xpath = "//div[@id='nation_code_inputfileddiv']//li")
+	public List<WebElement> nationalityOptions;
+	
+	@FindBy(xpath = "//input[@id='emp_dri_lice_exp_date']/following-sibling::span[1]//i")
+	public WebElement licExpDate;
+	
+	@FindBy(xpath = "//input[@id='emp_dri_lice_exp_date']/following-sibling::span[1]//div[@class='select-wrapper picker__select--year']/input")
+	public WebElement licExpYearInput;
+	
+	@FindBy(xpath = "//input[@id='emp_dri_lice_exp_date']/following-sibling::span[1]//div[@class='select-wrapper picker__select--year']//li")
+	public List<WebElement> licExpYearOptions;
+	
+	@FindBy(xpath = "//input[@id='emp_dri_lice_exp_date']/following-sibling::span[1]//div[@class='select-wrapper picker__select--month']/input")
+	public WebElement licExpMonthInput;
+	
+	@FindBy(xpath = "//input[@id='emp_dri_lice_exp_date']/following-sibling::span[1]//div[@class='select-wrapper picker__select--month']//li")
+	public List<WebElement> licExpMonthOptions;
+	
+	@FindBy(xpath = "//input[@id='emp_dri_lice_exp_date']/following-sibling::span[1]//td")
+	public List<WebElement> licExpDays;
+	
+	@FindBy(xpath = "//form[@id='pimPersonalDetailsForm']//button[@type='submit']")
+	public WebElement detailsSaveBtn;
 	
 	
 
--- src/test/java/com/neotech/steps/PersonalDetailsSteps.java
+++ src/test/java/com/neotech/steps/PersonalDetailsSteps.java
@@ -1,6 +1,9 @@
 package com.neotech.steps;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.neotech.utils.CommonMethods;
 
@@ -28,20 +31,78 @@ public class PersonalDetailsSteps extends CommonMethods {
 		//select gender
 		
 		//1st way: select Dropdown
-		selectDropdown(personalDetailsPage.genderDD, gender);
+	//	selectDropdown(personalDetailsPage.genderDD, gender);
 		
 		//2nd way: click and select (click)
-		click(personalDetailsPage.genderInput);
-		click(driver.findElement(By.xpath("//span[text()='"+gender+ "']")));
+	//	click(personalDetailsPage.genderInput);
+		//click on the element based on the gender string
+	//	click(driver.findElement(By.xpath("//span[text()='"+gender+ "']")));
+		
+		
+		//3rd way: click, get all options and loop to find the one to click on
+		selectDropdownByClicking(personalDetailsPage.genderInput, personalDetailsPage.genderOptions, gender);
+		
+		
+		//select nationality
+		selectDropdownByClicking(personalDetailsPage.nationalityInput, personalDetailsPage.nationalityOptions, nationality);
 		
 		
 		
+		//set the expiration date
+		
+		//this is our incoming format: 2026-02-08
+		String[] dateParts = expirationDate.split("-");
+		//dateParts = ["2026", "02", "08"]
+		
+		//lets click on the date icon
+		click(personalDetailsPage.licExpDate);
+		
+		//select year -- call selectDropdownByClicking( input ,   options,  value)
+		selectDropdownByClicking(personalDetailsPage.licExpYearInput, personalDetailsPage.licExpYearOptions, dateParts[0]);
+
+		//select month -- 
+		 //month is datePart[1] -- "02" ----> int 2
+		int month = Integer.parseInt(dateParts[1]);
+		click(personalDetailsPage.licExpMonthInput);
+		click(personalDetailsPage.licExpMonthOptions.get(month-1));
+		
+		
+		
+		int day = Integer.parseInt(dateParts[2]);
+		selectCalendarDate(personalDetailsPage.licExpDays, day+"");
+		
+		wait(2);
 	}
 	@Then("I click on Personal Details Save")
 	public void i_click_on_personal_details_save() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+
+		click(personalDetailsPage.detailsSaveBtn);
 	}
+	
+	/**
+	 * This method will click on the input tag of a dropdown and then get all the options, then 
+	 * would compare and click on the given option value.
+	 * 
+	 * @param input
+	 * @param options
+	 * @param value
+	 */
+	public void selectDropdownByClicking(WebElement input, List<WebElement> options, String value)
+	{
+		click(input);
+		
+		wait(1);
+		
+		for (WebElement option : options)
+		{
+			if (option.getText().equals(value))
+			{
+				click(option);
+				break;
+			}
+		}	
+	}
+	
 	
 	
 }
